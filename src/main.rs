9238ad8fde119ebb148caa05cabe0d346e9fdc77
sidereior <alexanderlnanda@gmail.com>
2022-12-09 09:52:30 -0500

    added dynamic size of points to select, need to fix vector to array

--- src/main.rs
+++ src/main.rs
@@ -34,16 +34,30 @@ fn main() {
     if let Some('\r')=hoft.chars().next_back() {
         hoft.pop();
     }
+    let mut stringEvals=String::new();
+    print!("How many points would you like to include in your graph:");
+    let _=stdout().flush();
+    stdin().read_line(&mut hoft).expect("You did not enter a correct vector-valued function, try again");//is this needed
+    if let Some('\n')=hoft.chars().next_back() {
+        hoft.pop();
+    }
+    if let Some('\r')=hoft.chars().next_back() {
+        hoft.pop();
+    }
+    //could you just choose a few traces such as all lines from y=x and rotate by some value and then use z values and draw circles
 
-    //set vales for xs
-    let mut xs = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.];
-    let mut ys = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.];
-    let mut zs = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.];
+    let numEvals = meval::eval_str(stringEvals).unwrap();
+    let numIter: i64 = numEvals as i64;
+    let numUsize: usize = numEvals as usize;
+    let mut xs = vec![0.; numUsize];
+    let mut ys = vec![0.; numUsize];
+    let mut zs = vec![0.; numUsize];
     //now need to add user input to specify the bounds of the graph
     //and need to add ability to set bounds between
-    for n in 0..14 {
+    //lowest smoothing curve or just some way to generate splines between areas
+    for n in 0..numIter {
         let mut m = n as i64;
-        m = m - 6;
+        m = m - numIter/2;
         let mut newfoft=foft.clone();
         let mut newgoft=goft.clone();
         let mut newhoft=hoft.clone();
@@ -53,11 +67,15 @@ fn main() {
         let myx = meval::eval_str(newfoft).unwrap();
         let myy = meval::eval_str(newgoft).unwrap();
         let myz = meval::eval_str(newhoft).unwrap();
-        xs[n]=myx as f64;
-        ys[n]=myy as f64;
-        zs[n]=myz as f64;
+        xs[n as usize]=myx as f64;
+        ys[n as usize]=myy as f64;
+        zs[n as uszie]=myz as f64;
         //why does cos or sin or tan not work
     }
+    //then use the points to produce lowest smoothing curves between points
+    let mut fts = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.];
+    let mut gts = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.];
+    let mut hts = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.];
     let plot = Plot3D::new((xs, ys, zs, "r-o"));
     plot.show();
 }
